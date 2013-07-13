22be93d8598fd6784e5ff3619b2e20224daf15af
dougkrause <dougkrause@192.168.0.70>
2013-07-12 18:03:45 -0600

    testing

---
+++ src/msse676client/handler/Wk3Class.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package msse676client.handler;
+
+/**
+ *
+ * @author dougkrause
+ */
+public class Wk3Class {
+    
+}
